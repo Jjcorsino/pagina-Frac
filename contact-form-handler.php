d15e03642cdc5c9d3dea05f5159ce69bf8363f3d
Jjcorsino <68306642+Jjcorsino@users.noreply.github.com>
2021-01-27 11:56:14 -0300

    fix?

---
+++ contact-form-handler.php
@@ -0,0 +1,24 @@
+<?php
+    $name = $_POST['name'];
+    $visitor_email = $_POST['email'];
+    $message = $_POST['message'];
+
+
+
+    $email_from = 'juliaan_44@outlook.com';
+
+    $email_subject = "Nueva consulta";
+
+    $email_body = "Nombre y apellido: $name.\n". 
+                    "Correo: $visitor_email.\n". 
+                    "mensaje: $message.";
+
+    $to = "luccas.nqn@hotmail.com";   
+
+    $headers = "From: $email_from \r\n";       
+    $headers .= "Reply-To: $visitor_email \r\n";
+    
+    mail($to,$email_subject,$email_body,$headers);
+    header("Location: index.html");
+
+?>
